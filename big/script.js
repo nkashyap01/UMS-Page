d488ad950f485d8b7cbba1ef0dd1362eaa70b72d
neha <nehakashyap1011@gmail.com>
2023-10-29 13:32:02 +0530

    Created UMS

---
+++ big/script.js
@@ -0,0 +1,87 @@
+const btn = document.getElementsByClassName("btn");
+
+const side = document.getElementsByClassName("sidebar");
+
+const importantLink = document.getElementsByClassName("important-links");
+const contentHeader = document.getElementsByClassName("content-header");
+const btnnav = document.getElementsByClassName("btn");
+
+btn[0].addEventListener("click", () => {
+  side[0].classList.toggle("testt");
+  importantLink[0].classList.toggle("important-links2");
+  contentHeader[0].classList.toggle("content-header2");
+  btnnav[0].classList.toggle("btn-nav2");
+});
+
+async function fetchData() {
+  const message = await fetch("https://dummyjson.com/products");
+  const usefulMessage = await message.json();
+
+  console.log(usefulMessage.products);
+
+  data = usefulMessage.products;
+
+  data.forEach((element) => {
+    const test = document.querySelector(".message-box .message-box-two");
+
+    const test2 = document.createElement("p");
+    const test4 = document.createElement("span");
+
+    test2.classList.add("krishna");
+    test4.classList.add("empty-box");
+
+    const textnode = document.createTextNode(element.description);
+
+    test2.appendChild(textnode);
+
+    test.appendChild(test4);
+    test.appendChild(test2);
+  });
+}
+
+fetchData();
+
+const sign = document.querySelector(".out .sign");
+
+sign.addEventListener("click", function () {
+  window.open("../index.html");
+});
+
+const select = document.querySelector(".list .krishna");
+
+const newPage = document.querySelector("#show");
+
+select.addEventListener("mouseover", function () {
+  newPage.style.display = "block";
+});
+
+select.addEventListener("mouseout", function () {
+  newPage.style.display = "none";
+});
+
+// for clicking whatsapp button
+const whatsappButton = document.querySelector(
+  ".important-links #whatsapp-button"
+);
+
+const whatsappDiv = document.querySelector(".important-links .whatsapp");
+
+whatsappButton.addEventListener("click", function () {
+  whatsappDiv.style.display = "block";
+});
+
+const whatsappCloseButton = document.querySelector(
+  ".important-links .whatsapp .close .close-button"
+);
+
+whatsappCloseButton.addEventListener("click", function () {
+  whatsappDiv.style.display = "none";
+});
+
+const closeButton2 = document.querySelector(
+  ".important-links .whatsapp .close-button2"
+);
+
+closeButton2.addEventListener("click", function () {
+  whatsappDiv.style.display = "none";
+});
---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const testBtn=document.getElementById('testbtn');
+
+console.log(testBtn);
+
+testBtn.addEventListener('click', function () {
+    window.open("/big/index.html");
+});
