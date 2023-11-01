6f0b665b33718f0d3a3bfc5fa040953dbe4d6556
neha <nehakashyap1011@gmail.com>
2023-11-01 14:34:18 +0530

    removed some comment

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// const testBtn = document.getElementById("testbtn");
-
-// console.log(testBtn);
-
-// testBtn.addEventListener("click", function () {
-//   window.open("/big/index.html");
-// });
-
 const testBtn = document.getElementById("testbtn");
-
-const username = "12208483";
-const password = "neha@123";
 
 testBtn.addEventListener("click", function () {
   const userid = document.querySelector(".edit .one form #userid").value;
